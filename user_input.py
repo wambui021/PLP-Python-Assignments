b4e8e22a1336d38a4f3c5b529913334aa4f8fdbc
Twambui021 <teresiahwambuikaberia@gmail.com>
2024-03-06 12:42:11 +0300

    first commit

---
+++ user_input.py
@@ -0,0 +1,8 @@
+name = input("What is your name? ")
+
+age = input("How old are you? ")
+
+location = input("Where do you live? ")
+
+message = f"Hello {name}, you are {age} years old and live in {location}."
+print(message)
